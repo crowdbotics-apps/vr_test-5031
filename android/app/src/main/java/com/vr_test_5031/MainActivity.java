06b0655fa3c64d0129cea0d81383b138c02345e0
Crowdbotics <team@crowdbotics.com>
2019-06-22 17:11:02 +0000

    initial commit

---
+++ android/app/src/main/java/com/vr_test_5031/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vr_test_5031;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "vr_test_5031";
+    }
+}
